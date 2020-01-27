267ea68e45504c3ee06d311ff76e1bc0a9f923d7
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2020-01-27 11:15:46 -0800

    Show correct data in article page

---
+++ components/article/InfoBox.js
@@ -0,0 +1,62 @@
+const InfoBox = article => {
+  const { authors, published_at, word_count, read_time } = article;
+  const readTimeMinutes = Math.round(read_time / 60);
+
+  return (
+    <div className="info">
+      <p id="authors"> By {formatAuthors(authors)} </p>
+      <p id="date"> Published on {formatDate(published_at)} </p>
+      <p id="words"> {word_count} words </p>
+      <p id="time"> {readTimeMinutes} minute read </p>
+      <style jsx>{`
+        .info {
+          border-bottom: solid 1px black;
+          text-align: center;
+        }
+
+        p {
+          margin-bottom: 0px;
+          display: inline-block;
+          color: grey;
+        }
+
+        #authors {
+          width: 25%;
+        }
+
+        #date {
+          width: 35%;
+        }
+
+        #words,
+        #time {
+          width: 10%;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+const formatDate = dateString => {
+  const date = new Date(dateString);
+  const dateOptions = { month: 'long', day: 'numeric', year: 'numeric' };
+  const timeOptions = { hour: '2-digit', minute: '2-digit' };
+
+  const formattedDate = date.toLocaleDateString('en-US', dateOptions);
+  const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+
+  return `${formattedDate} at ${formattedTime}`;
+};
+
+const formatAuthors = authors => {
+  const authorsLength = authors.length;
+
+  return authors.map(function(author, index) {
+    if (authorsLength - 1 === index) {
+      return author;
+    }
+    return `${author} and `;
+  });
+};
+
+export default InfoBox;
--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,7 +1,33 @@
 import fetch from 'isomorphic-unfetch';
+import Layout from '../../components/Layout';
+import InfoBox from '../../components/article/InfoBox';
 
 const Article = article => {
-  return <h1> {article.title} </h1>;
+  const { title, description, image_url, ...rest } = article;
+
+  return (
+    <Layout>
+      <div className="root">
+        <img src={image_url} />
+        <InfoBox {...rest} />
+        <div className="content">
+          <h1> {title} </h1>
+          <p> {description} </p>
+        </div>
+        <style jsx>{`
+          .root {
+            width: 80%;
+            margin: auto;
+          }
+
+          img {
+            width: 100%;
+            max-height: 300px;
+          }
+        `}</style>
+      </div>
+    </Layout>
+  );
 };
 
 Article.getInitialProps = async function(context) {
